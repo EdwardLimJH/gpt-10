6f02eecb1183a7047e5bf2d253ed29bfe098aee2
EdwardLJH <Edwardlim026@gmail.com>
2024-03-20 12:17:49 +0800

    Removed render template and return json string with multiple language:text pairs

--- Flask_app/blueprints/review/review.py
+++ Flask_app/blueprints/review/review.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, request
 import json
 
 review_bp = Blueprint('review', __name__, template_folder='templates')
@@ -8,18 +8,18 @@ llm_outputs = None
 @review_bp.route('/review', methods=['GET', 'POST'])
 def review():
     global llm_outputs
-    if request.method == 'POST':
-        updated_data = request.json
-        print(updated_data)
-        llm_outputs[updated_data["key"]] = updated_data["value"]
-        print(f"Im updating review data with the following : {updated_data}")
-        print(llm_outputs)
-        return render_template('review/review.html', data=llm_outputs)
+    # Assumes that "GET" method is invoked first with data params
     if request.method == 'GET':
         data_param = request.args.get('data')
         # Check if data_param is not None and then decode the JSON string
         if data_param:
             data = json.loads(data_param)
             llm_outputs = data
-            print(f"Im in review with the meeting minutes : {data}")
-        return render_template('review/review.html', data=data)
+            print(f"Im in review with the meeting minutes")
+            print(f"llm_outputs has been initialized with : {llm_outputs}")
+    if request.method == 'POST':
+        updated_data = request.json
+        llm_outputs[updated_data["key"]] = updated_data["value"] # Assuming singular edits. To be changed for multiple edits
+        print(f"Im updating review data with the following : {updated_data}")
+        print(llm_outputs)
+    return json.dumps(llm_outputs)
