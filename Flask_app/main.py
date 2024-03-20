7b142f4ee3593fb54c86b9ec4f5d86d9b96938e5
EdwardLJH <Edwardlim026@gmail.com>
2024-03-20 12:33:06 +0800

    Load .env file in main and blueprints can access environment variables

--- Flask_app/main.py
+++ Flask_app/main.py
@@ -5,7 +5,7 @@ from blueprints.review.review import review_bp
 from blueprints.get_LLM_response.get_LLM_response import get_LLM_response_bp
 from blueprints.confirmation.confirmation import confirmation_bp
 from blueprints.send_email.send_email import send_email_bp
-
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.secret_key = "gpt-10_A+For everyone!!"
@@ -18,4 +18,5 @@ app.register_blueprint(send_email_bp)
 
 
 if __name__ == "__main__":
+    load_dotenv()
     app.run(debug=True)
