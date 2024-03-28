46a04f17e3714c9c28132d812faab8d28d6995f9
Edward Lim <edwardlim026@gmail.com>
2024-03-29 00:14:17 +0800

    Added new route for translation and for /send_email to translate the english meeting minutes to other languages before sending out as raw text in the email body

--- Flask_app/blueprints/send_email/send_email.py
+++ Flask_app/blueprints/send_email/send_email.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, session
+import requests
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -13,8 +14,27 @@ def send_email():
     email_body = json_response.get("email_body")
     email_title = json_response.get("email_title")
 
-    # handle translation here? [KIV]
-    smtp_server = 'smtp-mail.outlook.com'
+    # handle translation here
+    emails_dict = {"english":email_body}
+    language_preferences = session["language_preferences"]
+    for language in language_preferences:
+        if language.lower() not in emails_dict:
+            print(language)
+            # post request to /translate_chat with language & email_body
+            request_dict = {'message':email_body, "language":language}
+            response = requests.post('http://localhost:5000/translate_chat', data=request_dict, cookies=request.cookies)
+            response_dict = response.json()
+            emails_dict[language.lower()] = response_dict.get("translated_email")
+    
+    for k,v in emails_dict.items():
+        print("="*35)
+        print("Language:",k)
+        print("="*35)
+        print(v)
+    
+    email_body = "".join(emails_dict.values())
+
+    smtp_server = 'smtp-mail.outlook.com' 
     smtp_port = 587
     smtp_username = 'gpt10.4213.1@outlook.com' # dotenv() to load in main.py?
     smtp_password = 'gpt10email'
@@ -22,7 +42,7 @@ def send_email():
     from_email = 'gpt10.4213.1@outlook.com'
     # Assume emails are in session["email_list"]
     # to_email_list = session.get("email_list", None) # get email list from session, # Ensure not None
-    to_email_list = ['gpt10.4213.1@outlook.com',] # Placeholder
+    to_email_list = ['gpt10.4213.1@outlook.com','brandonlim1014@gmail.com'] # Placeholder
 
     # meeting_date = "5/3/2024"
     # subject = f"{meeting_date} Meeting Minutes"  # meeting_date either user input or LLM search through document.
