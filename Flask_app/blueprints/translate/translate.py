21501ec82e1e6b48606b41d2e866844d1c7d96e0
Edward Lim <edwardlim026@gmail.com>
2024-04-20 18:20:28 +0800

    Added docstrings

--- Flask_app/blueprints/translate/translate.py
+++ Flask_app/blueprints/translate/translate.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, session, request, jsonify
-import json
 import os
-from h2ogpte import H2OGPTE
-from blueprints.get_LLM_response.prompts import TRANSLATION_SYSTEM_PROMPT, generate_translate_prompt
 from utils import rag_chat
+from h2ogpte import H2OGPTE
+from flask import Blueprint, request, jsonify
+from blueprints.get_LLM_response.prompts import TRANSLATION_SYSTEM_PROMPT, generate_translate_prompt
+
 
 H2O_API_KEY = os.getenv("H2O_API_KEY")  
 
@@ -13,7 +13,13 @@ translate_chat_bp = Blueprint('translate_chat', __name__)
 
 @translate_chat_bp.route('/translate_chat', methods=['POST'])
 def translate_chat():
-    print("Hi im at translate_chat")
+    """
+    Translates a chat message into the specified language using H2OGPTE.
+
+    Returns:
+        A JSON response containing the translated email content.
+    """
+    print("Currently at translate_chat")
     h2o_client = H2OGPTE(
         address='https://h2ogpte.genai.h2o.ai',
         api_key= H2O_API_KEY,
@@ -22,7 +28,6 @@ def translate_chat():
     language = request.form.get("language")
     chat_session_id = request.form.get("chat_session_id")
     translation_prompt = generate_translate_prompt(lang=language, message=message)
-    # return jsonify({"translated_email":message})
-    print(f"I am translating the email into {language}")
+    print(f"Translating the input into {language}")
     translated_email = rag_chat(h2o_client, chat_session_id, translation_prompt,TRANSLATION_SYSTEM_PROMPT)
     return jsonify({"translated_email":translated_email.content})
